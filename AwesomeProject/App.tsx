66f1a8590ceb0720b504849dbf8be38d05b1b808
Sneha kapoor <snehakapoor1193@gmail.com>
2023-09-25 17:29:12 +0530

    Initial commit

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import UserData from './components/UserData';
+import {
+ 
+  Text,
+  View,
+} from 'react-native';
+
+
+
+const App = () =>  {
+  
+  return (
+    <View>
+      <Text>Hello Sneha Kapoor </Text>
+      <UserData/>
+    </View>
+  );
+}
+
+export default App;
---
+++ AwesomeProject/components/UserData.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+
+const UserData =() => {
+    return(
+      <View>
+        <Text style={{fontSize: 20}}>Welcome</Text>
+        <Text style={{fontSize: 20}}>Age: 21</Text>
+        <Text style={{fontSize: 20}}>sk@gmail.com</Text>
+      </View>
+    )
+  }
+
+  export default UserData;
